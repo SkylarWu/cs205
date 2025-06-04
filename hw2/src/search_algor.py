25a4850c6dbe60af224895f4c613ae0064d91585
wuzhuoqian <wuzhuoqian@MacBookPro.lan>
2025-06-03 21:05:09 -0700

    update: comment

--- hw2/src/search_algor.py
+++ hw2/src/search_algor.py
@@ -13,8 +13,8 @@ def forward_selection(data, total_feature):
         best_feature_acc = 0.0
 
         for feature in left:
-            test_feature = selected + [feature]
-            acc = loocv(data, test_feature)
+            test_feature = selected + [feature] # adding this feature
+            acc = loocv(data, test_feature)  # Evaluate using LOOCV
 
             level_history.append((test_feature[:], acc))
 
@@ -22,8 +22,9 @@ def forward_selection(data, total_feature):
                 best_feature = feature
                 best_feature_acc = acc
 
-        history.append(level_history)
+        history.append(level_history)  # Save this level's test results
 
+        # if a better feature was found, keep it and continue
         if best_feature is not None and best_feature_acc > best_accuracy:
             selected.append(best_feature)
             left.remove(best_feature)
@@ -40,13 +41,14 @@ def backward_elimination(data, total_features):
     history = [[(selected[:], best_accuracy)]]
 
     while len(selected) > 1:
-        level_history = []
+        level_history = [] # stores all feature combinations tested at this level
         worst_feature = None
         best_feature_acc = 0.0
 
         for feature in selected:
-            test_feature = [f for f in selected if f != feature]
+            test_feature = [f for f in selected if f != feature] # removing this feature
             acc = loocv(data, test_feature)
+
             level_history.append((test_feature[:], acc))
 
             if acc > best_feature_acc:
@@ -55,6 +57,7 @@ def backward_elimination(data, total_features):
 
         history.append(level_history)
 
+        # if accuracy improves, remove the feature
         if best_feature_acc > best_accuracy:
             selected.remove(worst_feature)
             best_accuracy = best_feature_acc
